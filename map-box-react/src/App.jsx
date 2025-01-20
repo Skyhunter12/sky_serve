e6b93b7087b0f5f0682ed3e09e4fdeda1f9f0bcf
skyhunter12 <akashbagali043@gmail.com>
2025-01-20 12:39:54 +0530

    remove add new

---
+++ map-box-react/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Map from "./components/Map";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
+}
+
+export default App;
